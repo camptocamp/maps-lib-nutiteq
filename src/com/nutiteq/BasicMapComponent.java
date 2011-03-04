7c350828d7ccacfbb86c7e787e1c9048b53dfa84
Stéphane Brunner <stephane.brunner@camptocamp.com>
2011-03-04 16:28:26 +0100

    wrong import

--- src/com/nutiteq/BasicMapComponent.java
+++ src/com/nutiteq/BasicMapComponent.java
@@ -9,7 +9,6 @@ import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
 import javax.microedition.midlet.MIDlet;
 
-import com.camptocamp.android.gis.providers.SwisstopoMap;
 import com.mgmaps.cache.ScreenCache;
 import com.mgmaps.utils.Tools;
 import com.nutiteq.cache.Cache;
