346b8903938834337cc85b3924807f7cb310cef3
Aubort Jean-Baptiste <jean-baptiste.aubort@camptocamp.com>
2011-04-13 10:22:59 +0200

    Some static method must be public

--- android/src/com/nutiteq/android/MotionEventWrap.java
+++ android/src/com/nutiteq/android/MotionEventWrap.java
@@ -7,15 +7,15 @@ public class MotionEventWrap {
     private static final boolean IS_API_5 =  Integer.parseInt(Build.VERSION.SDK) >= 5;
 
     private MotionEventWrap(){};
-    static int getPointerCount(MotionEvent event) {
+    public static int getPointerCount(MotionEvent event) {
         return IS_API_5 ? MotionEventWrapNew.getPointerCount(event) : 1;
     }
 
-    static float getX(MotionEvent event, int idx) {
+    public static float getX(MotionEvent event, int idx) {
         return IS_API_5 ? MotionEventWrapNew.getX(event, idx) : 0;
     }
 
-    static float getY(MotionEvent event, int idx) {
+    public static float getY(MotionEvent event, int idx) {
         return IS_API_5 ? MotionEventWrapNew.getY(event, idx) : 0;
     }
 }
